68a308d4f50d6a07d9ac0438fc26a1790299eaa1
Aafaq Ahmad <aafaq.ahmad@opalina.in>
2018-11-28 15:35:24 +0530

    Updates

--- precache-manifest.ff27ef82e0b586d9c5d6cf79f47c8ee1.js
+++ precache-manifest.ff27ef82e0b586d9c5d6cf79f47c8ee1.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/task_mcr/static/js/runtime~main.d228cf7c.js"
   },
   {
-    "revision": "946f02b243c14908cfce",
-    "url": "/task_mcr/static/js/main.946f02b2.chunk.js"
+    "revision": "018672e55ed532122eb6",
+    "url": "/task_mcr/static/js/main.018672e5.chunk.js"
   },
   {
     "revision": "42856c432e7eddade212",
     "url": "/task_mcr/static/js/1.42856c43.chunk.js"
   },
   {
-    "revision": "946f02b243c14908cfce",
+    "revision": "018672e55ed532122eb6",
     "url": "/task_mcr/static/css/main.4cff4253.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/task_mcr/static/css/1.ee803262.chunk.css"
   },
   {
-    "revision": "0692f2328846c24376736ce587fe4d84",
+    "revision": "5191075b5a2b5e5638a1c33a54b3e74a",
     "url": "/task_mcr/index.html"
   }
 ];
